b41f03ca43d804614bebe6a35dbf9a32d2072ec3
sonkyro <accounts.b@emodea.de>
2026-01-28 21:34:29 +0100

    Adapted new styling to TrueFalse Editor

--- js/editor/renderers/trueFalse.js
+++ js/editor/renderers/trueFalse.js
@@ -1,29 +1,65 @@
 import {
   createWrapper,
-  input,
+  inputQuestion,
   deleteButton
 } from "./common.js";
+import { initStyle } from "../../utils.js";
 
 export function renderTrueFalse(question, index, onDelete) {
   const div = createWrapper("True / False");
 
-  const qInput = input("Frage", question.question, v => question.question = v);
+  const row = document.createElement("div");
+  row.className = "flex gap-2 mb-4 p-1";
+  const cCol = document.createElement("div");
+  cCol.className = "flex flex-col gap-1 flex-1";
+  const qInput = inputQuestion("Frage", question.question, v => question.question = v);
 
+  const dCol = document.createElement("div");
+  dCol.className = "justify-center";
+  dCol.style.width = "10%";
+  dCol.style.minWidth = "80px";
+
+  const selectStyle = ["h-full", "w-full", "text-center", "cursor-pointer", "outline-none"]
   const select = document.createElement("select");
-  select.className = "border p-1";
+  initStyle(select, "menuBtn", "green", "hover");
+  select.classList.add(...selectStyle);
 
   const optTrue = new Option("wahr", "wahr");
   const optFalse = new Option("falsch", "falsch");
-
   select.append(optTrue, optFalse);
+
   select.value = question.answer || "wahr";
 
   select.onchange = e => question.answer = e.target.value;
 
+  function updateSelectColor(isCorrect) {
+    if (isCorrect === "wahr") {
+        initStyle(select, "menuBtn", "green", "hover");
+    } else if (isCorrect === "falsch") {
+        initStyle(select, "menuBtn", "red", "hover");
+    } else {
+        initStyle(select, "menuBtn", "light-gray", "hover");
+    }
+     select.classList.add(...selectStyle);
+  }
+
+  select.onchange = e => {
+    const value = e.target.value;
+    question.userAnswer = value;
+    updateSelectColor(value);
+  };
+
+  cCol.append(qInput);
+  dCol.append(select);
+  row.append(cCol, dCol);
+
+  const deleteDiv = document.createElement("div");
+  deleteDiv.className = "mt-2 ml-1 flex justify-start";
+  deleteDiv.append(deleteButton(index, onDelete))
+
   div.append(
-    qInput,
-    select,
-    deleteButton(index, onDelete)
+    row,
+    deleteDiv
   );
 
   return div;
