1f5eeb32e734e39d272689edd5929306d8f10a26
sonkyro <accounts.b@emodea.de>
2026-01-28 20:12:11 +0100

    added new input areas for question, title, line and text

--- js/editor/renderers/common.js
+++ js/editor/renderers/common.js
@@ -1,3 +1,5 @@
+import { initStyle } from "../../utils.js";
+
 export function createWrapper(title) {
   const div = document.createElement("div");
   div.className = "border p-3 rounded bg-gray-50 space-y-2";
@@ -10,35 +12,84 @@ export function createWrapper(title) {
   return div;
 }
 
-export function input(placeholder, value, onChange) {
+export function inputQuestion(placeholder, value, onChange) {
   const i = document.createElement("input");
-  i.className = "border p-1 w-full";
+  i.className = "border rounded p-1 w-full";
   i.placeholder = placeholder;
   i.value = value || "";
   i.oninput = e => onChange(e.target.value);
   return i;
 }
 
-export function addButton(fn) {
+export function inputTitle(placeholder, value, onChange) {
+  const i = document.createElement("input");
+  i.className = "border rounded p-1 w-full text-l font-bold mb-1";
+  i.placeholder = placeholder;
+  i.value = value || "";
+  i.oninput = e => onChange(e.target.value);
+  return i;
+}
+
+export function inputLine(placeholder, value, onChange) {
+  const i = document.createElement("input");
+  i.className = "border rounded px-1 flex-1";
+  i.placeholder = placeholder;
+  i.style.minWidth = "40%";
+  i.style.width = "60%";
+  i.style.maxWidth = "80%"; 
+  i.value = value || "";
+  i.oninput = e => onChange(e.target.value);
+  return i;
+}
+
+export function inputText(placeholder, value, onChange) {
+  const i = document.createElement("textarea");
+  i.className = "border rounded p-1 flex-1 resize-none";
+  i.placeholder = placeholder;
+  i.style.height = "9em";
+  i.style.minHeight = "9em";  
+  i.style.overflowY = "auto";
+  i.value = value || "";
+  i.oninput = e => onChange(e.target.value);
+  return i;
+}
+
+export function addElBtn (fn, content = null) {
   const b = document.createElement("button");
-  b.textContent = "+ Hinzufügen";
-  b.className = "text-blue-600";
+  b.textContent = content || "Hinzufügen";
+  initStyle(b, "menuBtn", "blue", "hover");
   b.onclick = fn;
   return b;
 }
 
-export function delBtn(fn) {
+export function delElBtn(fn) {
   const b = document.createElement("button");
   b.textContent = "✕";
-  b.className = "text-red-500";
+  initStyle(b, "menuBtn", "red", "hover");
+  b.classList.remove("px-4", "py-2");
+  b.classList.add("px-2", "mr-2", "m-1")
   b.onclick = fn;
-  return b;
+  const delCol = document.createElement("div");
+  delCol.className = "flex flex-col justify-center";
+  delCol.append(b);
+  return delCol;
 }
 
 export function deleteButton(index, onDelete) {
   const b = document.createElement("button");
   b.textContent = "Frage löschen";
-  b.className = "text-red-600 mt-2";
+  initStyle(b, "menuBtn", "red", "hover");
   b.onclick = () => onDelete(index);
   return b;
+}
+
+export function addRemove(fn, index, onDelete) {
+  const addRemove = document.createElement("div");
+  addRemove.className = "mt-2 flex justify-between"
+  const addBtn = addElBtn(fn, "Absatz Hinzufügen");
+
+  const delQ = deleteButton(index, onDelete);
+
+  addRemove.append(addBtn, delQ)
+  return addRemove;
 }
